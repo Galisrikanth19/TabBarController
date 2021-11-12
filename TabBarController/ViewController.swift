fba4e8a53281a34a055114fcb3b55ee14d2282d7
Srikanth <Srikanth.gali@elorce.com>
2021-11-12 19:27:04 +0530

    Custom tabbar implemention for ios 15

--- TabBarController/ViewController.swift
+++ TabBarController/ViewController.swift
@@ -8,22 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let appearance = UITabBarAppearance()
-//        appearance.backgroundColor = .red
-//        //tabBar.standardAppearance = appearance
-//        self.tabBarController?.tabBar.standardAppearance = appearance
-        
-        
-        self.tabBarController?.tabBar.tintColor = .white
-        self.tabBarController?.tabBar.unselectedItemTintColor = .red
-        self.tabBarController?.tabBar.isTranslucent = false
-        
-        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.red], for: .normal)
-        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
-        
+        setTabBarAppearance()
     }
+    
+    private func setTabBarAppearance() {
+        if #available(iOS 15.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithDefaultBackground()
+            
+            let img = UIImage(named: "navigation_bg_image")
+            appearance.backgroundColor = UIColor(patternImage: img!)
+            
+            appearance.compactInlineLayoutAppearance.normal.iconColor = .white
+            appearance.compactInlineLayoutAppearance.normal.titleTextAttributes = [.foregroundColor : UIColor.white]
+            
+            appearance.inlineLayoutAppearance.normal.iconColor = .white
+            appearance.inlineLayoutAppearance.normal.titleTextAttributes = [.foregroundColor : UIColor.white]
+            
+            appearance.stackedLayoutAppearance.normal.iconColor = .white
+            appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor : UIColor.white]
+            
+            self.tabBarController?.tabBar.standardAppearance = appearance
+            self.tabBarController?.tabBar.scrollEdgeAppearance = self.tabBarController?.tabBar.standardAppearance
+            self.tabBarController?.tabBar.tintColor = .green
+            
+        } else {
+            let img = UIImage(named: "navigation_bg_image")
+            self.tabBarController?.tabBar.barTintColor =  UIColor(patternImage: img!)
+            self.tabBarController?.tabBar.unselectedItemTintColor = .white
+            self.tabBarController?.tabBar.tintColor = .green
+        }
+    }
+    
 }
